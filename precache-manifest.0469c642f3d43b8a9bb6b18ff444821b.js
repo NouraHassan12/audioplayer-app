af4bdd1b7ae031a093d2363017a5f6f2c8923d54
Noura Hassan <54744761+NouraHassan12@users.noreply.github.com>
2020-10-22 03:34:10 +0200

    Updates

---
+++ precache-manifest.0469c642f3d43b8a9bb6b18ff444821b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209c01be49062e724446eff5986cbffd",
+    "url": "/audioplayer-app/index.html"
+  },
+  {
+    "revision": "a62af25d3244d48eb628",
+    "url": "/audioplayer-app/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "a853e5eb038c4e4d9e82",
+    "url": "/audioplayer-app/static/css/main.fc7b07c4.chunk.css"
+  },
+  {
+    "revision": "a62af25d3244d48eb628",
+    "url": "/audioplayer-app/static/js/2.79f124dc.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/audioplayer-app/static/js/2.79f124dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a853e5eb038c4e4d9e82",
+    "url": "/audioplayer-app/static/js/main.c16349e1.chunk.js"
+  },
+  {
+    "revision": "5b394ef987fa21b08cb9",
+    "url": "/audioplayer-app/static/js/runtime-main.a5df3d96.js"
+  }
+]);
